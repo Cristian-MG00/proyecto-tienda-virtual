9543aeb1006940128ec95e7c4bbcbf5908a9d31f
Cristian-MG00 <cristian_carp2000@hotmail.com>
2025-08-06 12:28:40 -0300

    add: Estructura base del Footer terminada.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,35 @@
+const Footer = () => {
+  return (
+    <>
+      <div>
+        <img src="./tienda-virtual-icon.jpg" alt="Ícono de tienda virtual" />
+
+        <h4>
+          <a href="#">Política de privacidad</a>•
+          <a href="#">Política de cookies</a>
+        </h4>
+
+        <div>
+          <a href="https://www.instagram.com/">
+            <img src="./instagram-icon.webp" alt="Ícono de instagram" />
+          </a>
+          <a href="https://ar.linkedin.com/">
+            <img src="linkedin-icon.png" alt="Ícono de linkedín" />
+          </a>
+          <a href="https://www.facebook.com/?locale=es_LA">
+            <img src="facebook-icon.jpg" alt="Ícono de Facebook" />
+          </a>
+          <a href="https://x.com/?lang=es">
+            <img src="./x-icon.png" alt="Ícono de X" />
+          </a>
+        </div>
+      </div>
+      <p>
+        Sitio desarrollado por Cristian Machaca Gutierrez. © 2025 Tienda
+        Virtual. Todos los derechos reservados.
+      </p>
+    </>
+  );
+};
+
+export { Footer };
