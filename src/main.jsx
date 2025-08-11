faa0e8a1347fefa3f9b9d5facd88f84c6022d23c
Cristian-MG00 <cristian_carp2000@hotmail.com>
2025-08-10 22:00:00 -0300

    add: estructura básica de Login, todavía sin dar acceso a la cuenta de usuario

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Home } from "./pages/Home";
 import { Dashboard } from "./pages/Dashboard";
+import { Login } from "./pages/Login";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Dashboard />
+    <Login />
   </StrictMode>
 );
---
+++ src/pages/Login.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [succes, setSucces] = useState("");
+
+  const hundleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch("https://fakestoreapi.com/auth/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, password }),
+    });
+    if (response.ok) {
+      const token = await response.json();
+      // dar acceso al usuario a su cuenta
+    } else {
+      setError("El usuario no existe");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Ingresa a tu cuenta</h2>
+      <div>
+        <h4>Datos de prueba</h4>
+        <p>Nombre de usuario: hopkins</p>
+        <p>Contraseña: William56$hj</p>
+      </div>
+      <form onSubmit={hundleSubmit}>
+        <label>
+          Nombre de usuario:
+          <input type="text" onChange={(e) => setUsername(e.target.value)} />
+        </label>
+        <label>
+          Contraseña:
+          <input
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button>Ingresar</button>
+        {error && <p>{error}</p>}
+        {succes && <p>{succes}</p>}
+      </form>
+    </div>
+  );
+};
+
+export { Login };
