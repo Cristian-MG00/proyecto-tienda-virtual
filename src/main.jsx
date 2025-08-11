8d349c9c8991c965190834e3fcb283670ab91d69
Cristian-MG00 <cristian_carp2000@hotmail.com>
2025-08-11 11:12:40 -0300

    add: contenido estático de AboutUs. Enrutado de páginas con RouterApp

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { Home } from "./pages/Home";
-import { Dashboard } from "./pages/Dashboard";
-import { Login } from "./pages/Login";
-import { Register } from "./pages/Register";
-import { Layout } from "./components/Layout";
+import { RouterApp } from "./router/RouterApp";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Layout />
+    <RouterApp />
   </StrictMode>
 );
---
+++ src/router/RouterApp.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "../pages/Home";
+import { Dashboard } from "../pages/Dashboard";
+import { Login } from "../pages/Login";
+import { Register } from "../pages/Register";
+import { NotFound } from "../pages/NotFound";
+
+const RouterApp = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/notfound" element={<NotFound />} />
+        <Route path="/about-us" element={<AboutUs />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export { RouterApp };
